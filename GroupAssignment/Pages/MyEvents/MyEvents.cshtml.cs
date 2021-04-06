dc351e430c92ef3cb3cf04dc04d6d2d23cd5bd91
Cederlund1999 <cederlund1999@gmail.com>
2021-04-06 12:43:49 +0200

    MyEvents fungerar nu och visar vilka events usern har joinat.

--- GroupAssignment/Pages/MyEvents/MyEvents.cshtml.cs
+++ GroupAssignment/Pages/MyEvents/MyEvents.cshtml.cs
@@ -7,23 +7,34 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using GroupAssignment.Data;
 using GroupAssignment.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace GroupAssignment.Pages.MyEvents
 {
     public class IndexModel : PageModel
     {
-        private readonly GroupAssignment.Data.GroupAssignmentContext _context;
+        private readonly GroupAssignmentContext _context;
+        private readonly UserManager<MyUser> _userManager;
 
-        public IndexModel(GroupAssignment.Data.GroupAssignmentContext context)
+        public IndexModel(GroupAssignmentContext context, UserManager<MyUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
+        
+        
 
         public IList<Event> Event { get;set; }
 
         public async Task OnGetAsync()
         {
-            Event = await _context.Event.ToListAsync();
+            var thisUserId = _userManager.GetUserId(User);
+            var thisUser = await _context.MyUser.Where(x => x.Id == thisUserId)
+                .Include(z => z.JoinedEvents).FirstOrDefaultAsync();
+            Event = thisUser.JoinedEvents;
+
+
+            
         }
     }
 }
