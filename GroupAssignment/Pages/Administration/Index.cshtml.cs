8bf2c673c29232c69cfbe43db4607ef1ec0225e8
Palmeuh <fredrik.palme@utb.ecutbildning.se>
2021-04-07 11:48:27 +0200

    Added a role-administration page.

--- GroupAssignment/Pages/Administration/Index.cshtml.cs
+++ GroupAssignment/Pages/Administration/Index.cshtml.cs
@@ -1,15 +1,9 @@
-using System;
 using System.Collections.Generic;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using GroupAssignment.Data;
 using GroupAssignment.Models;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 
 namespace GroupAssignment.Pages.MyUsers
@@ -35,6 +29,8 @@ namespace GroupAssignment.Pages.MyUsers
         public async Task OnGetAsync()
         {
             Users = await _userManager.Users.ToListAsync();
+
+            
 
         }
 
