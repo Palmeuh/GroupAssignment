2924f2dad739151b67360ec2bb1b14769fe2b847
Palmeuh <fredrik.palme@utb.ecutbildning.se>
2021-04-07 14:37:56 +0200

    ManageUsers now display a list of users with their role

--- GroupAssignment/Models/MyUser.cs
+++ GroupAssignment/Models/MyUser.cs
@@ -5,7 +5,8 @@ using System.Collections.Generic;
 namespace GroupAssignment.Models
 {
     public class MyUser : IdentityUser
-    {
+    {       
+
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public List<Event> JoinedEvents { get; set; }
--- GroupAssignment/Pages/Administration/ManageUsers.cshtml.cs
+++ GroupAssignment/Pages/Administration/ManageUsers.cshtml.cs
@@ -5,8 +5,9 @@ using Microsoft.EntityFrameworkCore;
 using GroupAssignment.Data;
 using GroupAssignment.Models;
 using Microsoft.AspNetCore.Identity;
+using System.Linq;
 
-namespace GroupAssignment.Pages.MyUsers
+namespace GroupAssignment.Pages.ManageUsers
 {
     public class IndexModel : PageModel
     {
@@ -15,6 +16,13 @@ namespace GroupAssignment.Pages.MyUsers
         private readonly RoleManager<IdentityRole> _roleManager;
         private readonly GroupAssignmentContext _groupAssignmentContext;
 
+        public IEnumerable<MyUser> Users { get; set; }
+
+        public IEnumerable<IdentityRole> Roles { get; set; }
+
+        public Dictionary<MyUser, string> UserRoles { get; set; }
+
+
         public IndexModel(UserManager<MyUser> usermanager,
             RoleManager<IdentityRole> roleManager,
             GroupAssignmentContext groupAssignmentContext)
@@ -22,15 +30,37 @@ namespace GroupAssignment.Pages.MyUsers
             _userManager = usermanager;
             _roleManager = roleManager;
             _groupAssignmentContext = groupAssignmentContext;
+            UserRoles = new Dictionary<MyUser, string>();
         }
 
-        public IEnumerable<MyUser> Users { get; set; }
+       
+
+
 
         public async Task OnGetAsync()
         {
             Users = await _userManager.Users.ToListAsync();
+            Roles = await _roleManager.Roles.ToListAsync();            
 
-            
+            var users = await _userManager.Users.ToListAsync();            
+            foreach (var u in users)
+            {
+               
+                var roles = await _userManager.GetRolesAsync(u);
+                
+                foreach (var r in roles)
+                {
+                    UserRoles.Add(u, r);
+                }               
+            }
+
+
+
+
+
+
+
+
 
         }
 
